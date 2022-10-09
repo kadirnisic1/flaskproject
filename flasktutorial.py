ca38b8cced14c65ae9a76df0f8dff8fefb8f1f23
kadirnisic1 <kadir.nisic4@gmail.com>
2022-10-09 19:18:09 +0200

    adedflasktutorial.py

---
+++ flasktutorial.py
@@ -0,0 +1,28 @@
+from ast import While
+from flask import Flask, redirect, url_for
+
+app= Flask(__name__)
+a=False
+b=input("Uneiste lozinku:")
+while True:
+
+    if b=="kaciservis":
+        print("Tacno")
+        break
+    else:
+        print("netacno") 
+@app.route("/")
+def home():
+    return "Hello!!<h1>Hello</h1>"
+
+@app.route("/<name>")
+
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/admin/")
+def admin():
+    return redirect(url_for("user", name="Admin!"))
+
+if __name__=="__main__":
+    app.run()
